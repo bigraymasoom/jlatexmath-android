66e1dab9e57b8466a3e8f04050fb645784cc07cc
bigraymasoom <34131379+bigraymasoom@users.noreply.github.com>
2025-08-21 15:28:07 +0500

    Update JLatexMathAndroid.java

--- jlatexmath/src/main/java/ru/noties/jlatexmath/JLatexMathAndroid.java
+++ jlatexmath/src/main/java/ru/noties/jlatexmath/JLatexMathAndroid.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 
 public abstract class JLatexMathAndroid {
 
-    private static final String BASE = "org/scilab/forge/jlatexmath/";
+    private static final String BASE = "";;
 
     @SuppressLint("StaticFieldLeak")
     private static Context sContext = null;
